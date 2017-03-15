e55f5138c29e34b2a3a64ff4e09f4293e00763ee
Michael Grenon <mgrenon@emory.edu>
2017-03-15 04:39:02 -0400

    small fixes

--- index.js
+++
@@ -1,3 +0,0 @@
-function analyzeSentence(sentence) {
-
-}
---
+++ test.js
@@ -0,0 +1,35 @@
+/*
+ *
+ * This JS is essentially useless without a form and HTML input...
+ *
+ */
+
+/* toString(): converts 5-digit conjugation code to readable terms */
+function toString(z) {
+var q = conj.p[Math.floor(z/10000)]+" person ";
+q += conj.n[Math.floor((z/1000)%10)];
+q += conj.t[Math.floor((z/100)%10)];
+q += conj.v[Math.floor((z/10)%10)];
+q += conj.m[Math.floor(z%10)];
+return q;
+}
+
+/* prntConj() prints results of toString() */
+function prntConj(z) {
+document.writeln("The conjugation of "+z+" is:<br>"toString(z));
+}
+
+/* Conjugation object including all components of Latin verb syntax */
+var conj = {
+p: ["first ","second ","third "],
+n: ["singular ","plural "],
+t: ["present ","imperfect ","future ","perfect ","pluperfect ","future perfect "],
+v: ["active ","passive "],
+m: ["indicative ","subjunctive ", "imperative "]
+};
+
+/* Test conjugation code (must be manual for now) */
+var z = 11311;
+document.writeln("Input: "+z+"<br><br>");
+
+prntConj(z);
